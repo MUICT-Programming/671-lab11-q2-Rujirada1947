f6362019a0215ec9096ce8fbece45764d210074d
Rujirada1947 <rujirada.rak@student.mahidol.edu>
2024-10-20 20:49:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # YOUR CODE HERE
+def summation (lst1, Lst2):
+  list1 = []
+  for i in range(len(lst1)):
+    List1.append(Lst1[1] + lst2[i])
+    return listi
+    
+def find min_max(lst):
+  min_lst = min(lst) 
+  max_Lst = max(Lst) 
+  return min_lst, max_lst
+
+n = int(inputOD
+Lst1 = D
+for add in range(n):
+  Lst1. append intinput)))
+  
+Lst2 = []
+for add in range(n):
+  Lst2. append(int(input()))
+  
+current = summation (Lst1, Lst2)
+print (current)
+
+print(find_min_max (current))
